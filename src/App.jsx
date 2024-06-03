07bf61d0f10dd78750f40309e9f5c0f7bd42f126
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-06-03 23:47:33 +0530

    vercel routing is added

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,91 @@ import About from "./Component/About";
 import Skills from "./Component/Skills";
 import Contact from "./Component/Contect";
 import Footer from "./Component/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Skill from "./Component/Skills";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Header /> <Home /> <About /> <Skills />  <Contact /> <Project /> <Footer/>
-        </>
-      ),
-    },
-    {
-      path: "/About",
-      element: (
-        <>
-          <Header /> <About /> <Footer/>
-        </>
-      ),
-    },
-    {
-      path: "/Project",
-      element: (
-        <>
-          <Header /> <Project /> <Footer/>
-        </>
-      ),
-    },
-    {
-      path: "/skills",
-      element: (
-        <>
-          <Header /> <Skills /> <Footer/>
-        </>
-      ),
-    },
-    {
-      path: "/Contact",
-      element: (
-        <>
-          <Header /> <Contact /> <Footer/>
-        </>
-      ),
-    },
-  ]);
+  // const router = createBrowserRouter([
+
+  //   {
+  //     path: "/",
+  //     element: (
+  //       <>
+  //         <Header /> <Home /> <About /> <Skills />  <Contact /> <Project /> <Footer/>
+  //       </>
+  //     ),
+  //   },
+  //   {
+  //     path: "/About",
+  //     element: (
+  //       <>
+  //         <Header /> <About /> <Footer/>
+  //       </>
+  //     ),
+  //   },
+  //   {
+  //     path: "/Project",
+  //     element: (
+  //       <>
+  //         <Header /> <Project /> <Footer/>
+  //       </>
+  //     ),
+  //   },
+  //   {
+  //     path: "/skills",
+  //     element: (
+  //       <>
+  //         <Header /> <Skills /> <Footer/>
+  //       </>
+  //     ),
+  //   },
+  //   {
+  //     path: "/Contact",
+  //     element: (
+  //       <>
+  //         <Header /> <Contact /> <Footer/>
+  //       </>
+  //     ),
+  //   },
+  // ]);
+
   return (
     <>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home /> <About /> <Skills /> <Contact /> <Project />
+              </>
+            }
+          />
+          <Route
+            path="/About"
+            element={
+              <>
+                <About />
+              </>
+            }
+          />
+          <Route path="/skills" element = {
+            <>
+              <Skill/>
+            </>
+          } />
+          <Route path="/project" element = {
+            <>
+              <Project/>
+            </>
+          } /><Route path="/contact" element = {
+            <>
+              <Contact/>
+            </>
+          } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
