30444e49870d5fa884245d895ae72808063c14ee
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-07-05 00:03:38 +0530

    final commit

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import "./Header.css";
 import { NavLink } from "react-router-dom";
-import resume from "../Image/Piyush__Resume.pdf"
+import resume from "../Image/Piyush__Resume.pdf";
 
 function Header() {
   return (
     <>
       <nav>
-        <ul>
-          <NavLink  to="/"><li><span className="FirstWord">P</span>IYUSH</li></NavLink>
+        <ul className="flex items-center">
+          <NavLink to="/"><li><span className="FirstWord">P</span>IYUSH</li></NavLink>
           <NavLink className={(e) => e.isActive ? "Active" : ""} to="/"><li>Home</li></NavLink>
-          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/About"><li>About</li> </NavLink>{" "}
-          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/Project"><li>Project</li></NavLink>{" "}
-          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/skills"><li>Skills</li></NavLink>{" "}
-          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/Contact"><li>Contact</li> </NavLink>
-          <a href={resume} download>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  mybutoon " style={{alignSelf : "end"}}>
-              Download Resume
-            </button>
-          </a>
+          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/About"><li>About</li></NavLink>
+          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/Project"><li>Project</li></NavLink>
+          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/skills"><li>Skills</li></NavLink>
+          <NavLink className={(e) => e.isActive ? "Active" : ""} to="/Contact"><li>Contact</li></NavLink>
+          <div className="ml-auto">
+            <a href={resume} download>
+              <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">
+                Download Resume
+              </button>
+            </a>
+          </div>
         </ul>
       </nav>
     </>
